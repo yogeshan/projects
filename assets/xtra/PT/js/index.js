45d8686776d0940faa56072141bb91a1f0601fc1
yogeshan <yogi.chettiyar@gmail.com>
2014-07-06 14:16:51 +0530

    Deleted Everything

--- assets/xtra/PT/js/index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".prclist").hover(function() {
-        $(".prclist").removeClass("current");
-        $(this).toggleClass("current");
-    },function(){
-        $(".prclist").removeClass("current");
-    });
-});
